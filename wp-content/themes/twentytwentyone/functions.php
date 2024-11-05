05d2233818549189e1326c960a0bb107aa33147a
Ngô Ngọc Phước <133665547+nnp2612@users.noreply.github.com>
2024-11-06 00:02:56 +0700

    Comit footer

--- wp-content/themes/twentytwentyone/functions.php
+++ wp-content/themes/twentytwentyone/functions.php
@@ -655,3 +655,11 @@ if ( ! function_exists( 'wp_get_list_item_separator' ) ) :
 		return __( ', ', 'twentytwentyone' );
 	}
 endif;
+function my_theme_enqueue_styles() {
+    
+    wp_enqueue_style('style', get_stylesheet_uri());
+
+    wp_enqueue_style('custom-style', get_template_directory_uri() . '/custom-style.css');
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
