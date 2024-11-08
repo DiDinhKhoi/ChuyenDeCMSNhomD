71a1784eaaa9ebaaceb8e283cf55b5b534866914
tranquocviet.tdc2020 <tranquocviet.tdc2020@gmail.com>
2024-11-08 14:28:08 +0700

    up4

--- wp-content/themes/twentytwentyone/template-parts/content/content.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content.php
@@ -19,7 +19,7 @@
                 <span class="month">Tháng 10</span>
             </div>
             <div class="content">
-                <h3><a href="http://127.0.0.1:82/2024/10/30/nganh-truyen-thong-mang-may-tinh/" target="_blank">Lịch
+                <h3><a href="http://127.0.0.1:82/2024/11/07/177/" target="_blank">Lịch
                         Phỏng Vấn Chương Trình CNTT Nhật Bản 2021</a></h3>
                 <p>Các sinh viên có tên trong danh sách vui lòng có mặt theo lịch để tham dự phỏng vấn[...]</p>
             </div>
@@ -34,7 +34,7 @@
                 <span class="month">Tháng 12</span>
             </div>
             <div class="content">
-                <h3><a href="http://127.0.0.1:82/2024/10/30/nganh-truyen-thong-mang-may-tinh/" target="_blank">Messi lập
+                <h3><a href="http://127.0.0.1:82/2024/11/07/177/" target="_blank">Messi lập
                         cú đúp giúp inter Miami Vô địch MLS 2024</a></h3>
                 <p>Lionel Messi đã góp công lớn giúp Inter Miami lần đầu tiên giành chức vô địch Supporters' Shield khi
                       [...]</p>
