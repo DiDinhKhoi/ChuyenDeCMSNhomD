57bcb5b6e124f1c0c506ccbd046f11f324fd5b55
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-11-13 14:28:27 +0700

    up

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -9,36 +9,59 @@
  * @since Twenty Twenty-One 1.0
  */
 
-?>
+ ?>
+<div class="row">
+            <div class="col-md-3 bg-corossedbg">
+            <h4 class="categories-2">Categories</h4>
+                <div class="crossedbg-13">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <div class="br-13">
+                            <div class="crossedbg">
 
-	<header class="entry-header alignwide">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twenty_twenty_one_post_thumbnail(); ?>
-	</header><!-- .entry-header -->
+                            </div>
+                                <div class="row br9">
 
-	<div class="entry-content">
-		<?php
-		the_content();
+                                    <div class="categories">
+                                        <?php get_wp_categories(); ?>
+                                    </div>
+                                        <?php
+                                            function get_wp_categories() {
+                                                $categories = get_categories();
+                                                if ($categories) {
+                                                    echo '<ul>';
+                                                    foreach ($categories as $category) {
+                                                        echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
+                                                    }
+                                                echo '</ul>';
+                                                } else {
+                                                echo 'No categories found.';
+                                                }
+                                            }
+                                        ?>
+                            </div>
+                        </div>
+                
+            </div>
+        </div>
+        <div class="col-md-6">
+            
 
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
-
-	<?php if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
-	<?php endif; ?>
+ <div class="row">
+	 <div class="col-md-12"><div class="overviewline"></div></div>
+ </div>
+ <?php twenty_twenty_one_post_thumbnail(); ?>
+</header><!-- .entry-header -->
+<div class="entry-content" style="margin-bottom: 20px;">
+ <?php the_content(); ?>
+</div><!-- .entry-content -->
+<?php if ( ! is_singular( 'attachment' ) ) : ?>
+ <?php get_template_part( 'template-parts/post/author-bio' ); ?>
+<?php endif; ?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
+
+ </div>
+
+ </div>
+ 
+</div>
