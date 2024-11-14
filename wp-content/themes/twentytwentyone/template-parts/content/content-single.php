fb4f9184fb6377f9e8ffdc57b4ffe9045d72ad51
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-11-14 13:04:21 +0700

    fix

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -10,78 +10,58 @@
  */
 
  ?>
+<div class="row">
+            <div class="col-md-3 bg-corossedbg">
+            <h4 class="categories-2">Categories</h4>
+                <div class="crossedbg-13">
 
-<div class="detailpage" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="row">
-        <div class="col-md-3">
-        </div>
-        <div class="col-md-6">
-            <header class="entry-header alignwide pb-3 mb-0" style="border-bottom: 0;">
-                <div class="row title">
-                    <div class="col-md-10 col-xs-9">
-                        <?php the_title('<h1 class="entry-title font-weight-bold m-0 p-0" style="font-size: 2.1em">', '</h1>'); ?>
-                        <?php twenty_twenty_one_post_thumbnail(); ?>
-                    </div>
-                    <div class="col-md-2 col-xs-3">
-                        <div class="headlinesdate" id="date-title-module-6">
-                            <div class="headlinesdm">
-                                <div class=" headlinesday">
-                                    <?php the_time('d'); ?>
-                                </div>
-                                <div class="headlinesmonth">
-                                    <?php the_time('m'); ?>
-                                </div>
+                        <div class="br-13">
+                            <div class="crossedbg">
+
                             </div>
-                            <div class="headlinesyear">'
-                                <?php the_time('y'); ?>
+                                <div class="row br9">
+
+                                    <div class="categories">
+                                        <?php get_wp_categories(); ?>
+                                    </div>
+                                        <?php
+                                            function get_wp_categories() {
+                                                $categories = get_categories();
+                                                if ($categories) {
+                                                    echo '<ul>';
+                                                    foreach ($categories as $category) {
+                                                        echo '<li><a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
+                                                    }
+                                                echo '</ul>';
+                                                } else {
+                                                echo 'No categories found.';
+                                                }
+                                            }
+                                        ?>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="overviewline"></div>
-                    </div>
-                </div>
-            </header><!-- .entry-header -->
-            <!-- <div class="row overview"></div> -->
-            <!-- <div class="row overview_thumb"></div> -->
-            <div class="row maincontent">
+                
+            </div>
+        </div>
+        <div class="col-md-6">
+            
 
-            <div class="entry-content mt-0">
-    <div class="categories" style="width: 20%">
-    </div>
-    <?php
-        // Lấy nội dung của bài viết và áp dụng bộ lọc nội dung
-        $content = apply_filters('the_content', get_the_content());
+ <div class="row">
+	 <div class="col-md-12"><div class="overviewline"></div></div>
+ </div>
+ <?php twenty_twenty_one_post_thumbnail(); ?>
+</header><!-- .entry-header -->
+<div class="entry-content" style="margin-bottom: 20px;">
+ <?php the_content(); ?>
+</div><!-- .entry-content -->
+<?php if ( ! is_singular( 'attachment' ) ) : ?>
+ <?php get_template_part( 'template-parts/post/author-bio' ); ?>
+<?php endif; ?>
 
-        // Tách nội dung thành các đoạn dựa trên dấu xuống dòng
-        $paragraphs = preg_split('/(\r?\n)+/', $content);
+</article><!-- #post-<?php the_ID(); ?> -->
 
-        // Hiển thị đoạn đầu tiên với chữ nghiêng
-        if (!empty($paragraphs[0])) {
-            echo '<p class="font-italic">' . wp_kses_post($paragraphs[0]) . '</p>';
-        }
+ </div>
 
-        // Hiển thị các đoạn còn lại với chữ bình thường
-        for ($i = 1; $i < count($paragraphs); $i++) {
-            if (!empty(trim($paragraphs[$i]))) { // Bỏ qua đoạn rỗng
-                echo '<p>' . wp_kses_post($paragraphs[$i]) . '</p>';
-            }
-        }
-
-        // Hiển thị các trang nếu bài viết có nhiều trang
-        wp_link_pages(
-            array(
-                'before' => '<nav class="page-links" aria-label="' . esc_attr__('Page', 'twentytwentyone') . '">',
-                'after' => '</nav>',
-                'pagelink' => esc_html__('Page %', 'twentytwentyone'),
-            )
-        );
-    ?>
+ </div>
+ 
 </div>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-    </div><!-- #post-<?php the_ID(); ?> -->
