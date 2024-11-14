df2ca66c99f0b3c07feecc010e418c4d19485124
Ngô Ngọc Phước <133665547+nnp2612@users.noreply.github.com>
2024-11-14 10:17:26 +0700

    hoangthanh

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,161 +12,125 @@
  * @since Twenty Twenty-One 1.0
  */
 
- ?>
- </main><!-- #main -->
+?>
+<!-- #main -->
 </div><!-- #primary -->
 </div><!-- #content -->
 
-<head>
+<?php get_template_part('template-parts/footer/footer-widgets'); ?>
 
-</head>
+	
+
 <section id="footer">
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+	<div class="container">
+		<div class="row text-center text-xs-center text-sm-left text-md-left">
+			<div class="col-xs-12 col-sm-4 col-md-4">
+				<h5>Comment</h5>
+				<ul class="list-unstyled quick-links">
+					<?php
+					$comments = get_comments(array(
+						'status' => 'approve', // Lấy các bình luận đã được phê duyệt
+						'number' => 5, // Lấy 5 bình luậm
+					));
 
-<!-- Footer -->
-	<section id="footer">
-		<div class="container">
-			<div class="row text-center text-xs-center text-sm-left text-md-left">
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Videos</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-12 col-sm-4 col-md-4">
-					<h5>Quick links</h5>
-					<ul class="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Get Started</a></li>
-						<li><a href="https://wwwe.sunlimetech.com" title="Design and developed by"><i class="fa fa-angle-double-right"></i>Imprint</a></li>
-					</ul>
-				</div>
+					foreach ($comments as $comment) :
+						$comment_link = esc_url(get_comment_link($comment->comment_ID)); // Tạo liên kết đến bình luận
+						$comment_parent = $comment->comment_parent; // Lấy ID của bình luận cha
+						$post_id = $comment->comment_post_ID; // Lấy ID của bài viết mà bình luận thuộc về
+
+						// Lấy thông tin về bài viết
+						$post = get_post($post_id);
+
+						if ($comment_parent == 0) {
+							// Bình luận gốc
+							$post_title = mb_substr($post->post_title, 0, 20, 'UTF-8'); // Rút gọn tiêu đề bài viết
+							echo '<li><a style="text-decoration: underline !important;" href="' . $comment_link . '"><i class="fa fa-angle-double-right"></i>' . esc_html($comment->comment_content) . '</a>';
+							echo ' <span style="color: white">On</span> <a style="text-decoration: underline !important;"  href="' . get_permalink($post_id) . '">' . esc_html($post_title) . '</a></li>';
+						} else {
+							// Bình luận con
+							$parent_comment = get_comment($comment_parent); // Lấy thông tin của bình luận cha
+							$parent_comment_link = esc_url(get_comment_link($parent_comment->comment_ID)); // Liên kết đến bình luận cha
+
+							$post_title = mb_substr($post->post_title, 0, 20, 'UTF-8'); // Rút gọn tiêu đề bài viết
+							echo '<li><a style="text-decoration: underline !important;"  href="' . $comment_link . '">' . esc_html($comment->comment_content) . '</a>';
+							echo ' (Reply to <a style="text-decoration: underline !important;"  href="' . $parent_comment_link . '">' . esc_html($parent_comment->comment_content) . '</a>)';
+							echo ' <span style="color: white">On</span> <a style="text-decoration: underline !important;"  href="' . get_permalink($post_id) . '">' . esc_html($post_title) . '</a></li>';
+						}
+					endforeach;
+					?>
+				</ul>
 			</div>
-			<div class="row">
-	 <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-		 <ul class="list-unstyled list-inline social text-center">
-			 <p class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook-f"></i></a></p>
-			 <p class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-twitter"></i></a></p>
-			 <p class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-instagram"></i></a></p>
-			 <p class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-google-plus-g"></i></a></p>
-			 <p class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></p>
-		 </ul>
-	 </div>
-	 <hr>
- </div>	
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis, MN]</p>
-					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
-				</div>
-				<hr>
-			</div>	
+			<div class="col-xs-12 col-sm-4 col-md-4">
+				<h5>Categories</h5>
+				<?php
+				// Lấy danh sách danh mục
+				$categories = get_categories();
+
+				if ($categories) {
+					echo ' <ul class="list-unstyled quick-links">';
+
+					foreach ($categories as $category) {
+						echo '<li><a href="' . get_category_link($category->term_id) . '"><i class="fa fa-angle-double-right"></i>' . $category->name . '</a></li>';
+					}
+
+					echo '</ul>';
+				}
+				?>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4">
+
+				<h5>Last Posts</h5>
+				<ul class="list-unstyled quick-links">
+					<?php
+					// Truy vấn cơ sở dữ liệu để lấy 5 bài viết mới nhất
+					$args = array(
+						'post_type' => 'post',
+						'posts_per_page' => 5,
+					);
+
+					$query = new WP_Query($args);
+
+					if ($query->have_posts()) {
+						while ($query->have_posts()) {
+							$query->the_post();
+							echo '<li><a href="' . get_permalink() . '"><i class="fa fa-angle-double-right"></i>' . get_the_title() . '</a></li>';
+						}
+						wp_reset_postdata();
+					}
+					?>
+				</ul>
+
+			</div>
 		</div>
-	</section>
-	<!-- ./Footer -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+				<ul class="list-unstyled list-inline social text-center">
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-facebook"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-twitter"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-instagram"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-google-plus"></i></a></li>
+					<li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fa fa-envelope"></i></a></li>
+				</ul>
+			</div>
+			<hr>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+				<p><u><a href="https://www.nationaltransaction.com/">National Transaction Corporation</a></u> is a
+					Registered MSP/ISO of Elavon, Inc. Georgia [a wholly owned subsidiary of U.S. Bancorp, Minneapolis,
+					MN]</p>
+				<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Sunlimetech</a></p>
+			</div>
+			<hr>
+		</div>
+	</div>
 
-
+</section>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 </body>
+
 </html>
-<style>
-/* Footer */
-@import url('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-.container {
-	padding: 30px;
-}
-section .section-title {
-    text-align: center;
-    color: #007b5e;
-    margin-bottom: 50px;
-    text-transform: uppercase;
-}
-#footer {
-    background: #007b5e !important;
-}
-#footer h5{
-	padding-left: 10px;
-    border-left: 3px solid #eeeeee;
-    padding-bottom: 6px;
-    margin-bottom: 20px;
-    color:#ffffff;
-}
-#footer a {
-    color: #ffffff;
-    text-decoration: none !important;
-    background-color: transparent;
-    -webkit-text-decoration-skip: objects;
-}
-#footer ul.social li{
-	padding: 3px 0;
-}
-#footer ul.social li a i {
-    margin-right: 5px;
-	font-size:25px;
-	-webkit-transition: .5s all ease;
-	-moz-transition: .5s all ease;
-	transition: .5s all ease;
-}
-#footer ul.social li:hover a i {
-	font-size:30px;
-	margin-top:-10px;
-}
-#footer ul.social li a,
-#footer ul.quick-links li a{
-	color:#ffffff;
-}
-#footer ul.social li a:hover{
-	color:#eeeeee;
-}
-#footer ul.quick-links li{
-	padding: 3px 0;
-	-webkit-transition: .5s all ease;
-	-moz-transition: .5s all ease;
-	transition: .5s all ease;
-}
-#footer ul.quick-links li:hover{
-	padding: 3px 0;
-	margin-left:5px;
-	font-weight:700;
-}
-#footer ul.quick-links li a i{
-	margin-right: 5px;
-}
-#footer ul.quick-links li:hover a i {
-    font-weight: 700;
-}
-
-@media (max-width:767px){
-	#footer h5 {
-    padding-left: 0;
-    border-left: transparent;
-    padding-bottom: 0px;
-    margin-bottom: 10px;
-}
-}
-
-</style>
--- wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
+++ wp-content/themes/twentytwentyone/template-parts/footer/footer-widgets.php
@@ -15,13 +15,3 @@ if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 
 	<?php
 endif;
-
-//Thêm footer #2
-if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-
-    <aside class="widget-area footer-2">
-        <?php dynamic_sidebar( 'sidebar-1' ); ?>
-    </aside>
-
-<?php
-endif;
