420b194301a34a0514a3590790f43f882e114f6e
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-11-15 13:24:35 +0700

    fix

--- wp-content/themes/twentytwentyone/searchform.php
+++ wp-content/themes/twentytwentyone/searchform.php
@@ -21,11 +21,28 @@ $twentytwentyone_unique_id = wp_unique_id( 'search-form-' );
 
 $twentytwentyone_aria_label = ! empty( $args['aria_label'] ) ? 'aria-label="' . esc_attr( $args['aria_label'] ) . '"' : '';
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <label for="search-input" class="screen-reader-text">Search topics or keywords</label>
-    <input type="search" id="search-input" class="search-field" placeholder="Search topics or keywords" value="<?php echo get_search_query(); ?>" name="s" />
-    <button type="submit" class="search-submit">
-        Search
-    </button>
-</form>
+<div class="container">
+    <br/>
+	<div class="row justify-content-center">
+                        <div class="col-12 col-md-10 col-lg-12">
+                            <form class="card card-sm">
+                                <div class="card-body row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <i class="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col">
+                                        <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search topics or keywords">
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col-auto">
+                                        <button class="btn btn-lg btn-success" type="submit">Search</button>
+                                    </div>
+                                    <!--end of col-->
+                                </div>
+                            </form>
+                        </div>
+                        <!--end of col-->
+                    </div>
+</div>
 
